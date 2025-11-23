d8a0f435fd3466a865cd0d8d12a8d11a0a73463a
Andrew <reznik1224@gmail.com>
2025-11-23 18:34:49 +0200

    init

---
+++ src/js/render-function.js
@@ -0,0 +1,40 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  const gallery = document.querySelector('.gallery');
+
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <a class="gallery-item" href="${largeImageURL}">
+        <div class="photo-card">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item"><b>Likes:</b> ${likes}</p>
+            <p class="info-item"><b>Views:</b> ${views}</p>
+            <p class="info-item"><b>Comments:</b> ${comments}</p>
+            <p class="info-item"><b>Downloads:</b> ${downloads}</p>
+          </div>
+        </div>
+      </a>`
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+
+  lightbox.refresh();
+}
---
+++ src/main.js
@@ -0,0 +1,66 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './js/pixabay-api.js';
+import { createGallery } from './js/render-function.js';
+
+const form = document.querySelector('.form');
+const gallery = document.querySelector('.gallery');
+const input = form.querySelector('input[name="search-text"]');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const query = input.value.toLowerCase().trim();
+
+  gallery.innerHTML = '';
+
+  if (!query) {
+    iziToast.warning({
+      message: 'Please enter a search query!',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  loadImages(query);
+}
+
+function loadImages(query) {
+  showLoader();
+  getImagesByQuery(query)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      }
+
+      createGallery(data.hits);
+      input.value = '';
+    })
+    .catch(() => {
+      iziToast.error({
+        message: 'Something went wrong. Please try again later.',
+        position: 'topRight',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+    });
+}
+
+const loader = document.querySelector('.loader');
+
+function showLoader() {
+  loader.classList.remove('hidden');
+}
+
+function hideLoader() {
+  loader.classList.add('hidden');
+}
